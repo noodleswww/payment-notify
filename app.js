8d418f0c902fd267bb293af32dc1a726ce75eff9
noodleswww <boatwww@icloud.com>
2015-11-12 10:52:28 +0800

    inti2

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 
-app.all('/alipay/notify', function (req, res,next) {
+app.all('/aws/notifypaystatus', function (req, res,next) {
   console.log(req.headers);
+  console.log(req.query);
+  console.log(req.body);
+  res.end('index');
 });
 app.listen(9000, function () {
   console.log('start payment notify ok');

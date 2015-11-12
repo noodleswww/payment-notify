23dfc740cee45c4a42be5aa8b2a20a34498bf931
noodleswww <boatwww@icloud.com>
2015-11-12 12:02:16 +0800

    add parse

--- app.js
+++ app.js
@@ -10,6 +10,27 @@ app.all('/aws/notifypaystatus', function (req, res,next) {
   console.log(req.body);
   res.end('index');
 });
+
+app.use(function (req, res, next) {
+  console.log('parse');
+  // parse
+  var buf = '';
+  req.setEncoding('utf8');
+  req.on('data', function(chunk){ buf += chunk });
+  req.on('end', function(){
+    if(buf){
+      try{
+        var qs = require('querystring');
+        var ob = qs.decode(buf);
+        req.query = ob;
+      }catch (e){
+        console.log('taobao body parser fail!');
+        console.log(e);
+      }
+    }
+    next();
+  });
+});
 app.listen(9000, function () {
   console.log('start payment notify ok');
 });
